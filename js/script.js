b36c447e05279a329c7e200789ab1e4eafbee479
Rabiul Islam <74459680+rabiul-jamalpur@users.noreply.github.com>
2020-12-04 20:56:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var wow = new WOW(
+        {
+          boxClass:     'wow',     
+          animateClass: 'animated', 
+          offset:       0,          
+          mobile:       true,       
+          live:         true,     
+          callback:     function(box) {},
+          scrollContainer: null,   
+          resetAnimation: true,   
+        });
+      wow.init();
+});
